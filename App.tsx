cc0fe0879b2a69451f3fa093fe982e9ab1ebdb43
claitonneri <claitonneri@hotmail.com>
2024-03-04 23:30:20 -0300

    chore: config OneSignal

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { OneSignal } from 'react-native-onesignal'
 
 import { THEME } from './src/theme';
 import { Routes } from './src/routes';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+
+OneSignal.initialize("7e578caf-d7ba-4fed-b468-698e13c453e2")
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
