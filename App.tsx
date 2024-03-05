308ef12a6188f03209e6a3356d9409c8ba56a887
claitonneri <claitonneri@hotmail.com>
2024-03-04 22:59:33 -0300

    rnshoes-app

---
+++ App.tsx
@@ -0,0 +1,26 @@
+import { StatusBar } from 'react-native';
+import { NativeBaseProvider } from 'native-base';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { THEME } from './src/theme';
+import { Routes } from './src/routes';
+import { Loading } from './src/components/Loading';
+
+import { CartContextProvider } from './src/contexts/CartContext';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <CartContextProvider>
+        {fontsLoaded ? <Routes /> : <Loading />}
+      </CartContextProvider>
+    </NativeBaseProvider>
+  );
+}
